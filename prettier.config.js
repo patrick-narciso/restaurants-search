52621431733aff40edcb4da8657fe1fc2c3fcf0c
Patrick Narciso <patrick.ncsilva@gmail.com>
2020-06-21 22:32:03 -0300

    adiciona eslint, prettier e babel

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 100,
+  useTabs: false,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  endOfLine: 'lf',
+  arrowParens: 'always',
+};
