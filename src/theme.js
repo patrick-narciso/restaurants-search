294f7363df2c006c1e8518d136be1b26808962ec
wellingtonmont22 <wellington.m.alves@hotmail.com>
2021-07-31 12:55:58 -0300

    add Theme

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+
+
+const theme = {
+    colors: {
+        primary: '#6200ee',
+        background: '#00000014',
+        text: '#000000B3',
+        sucess: '#13501'
+    },
+    fonts: {
+        regular: 'Roboto, sans-serif',
+    },
+};
+
+export default theme;
